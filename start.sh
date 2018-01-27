0dfcd3d21ca524c9be94579ee76039b7496669ee
farrasrayhand <35839373+farrasrayhand@users.noreply.github.com>
2018-01-27 14:23:00 +0800

    Usefull Script

--- start.sh
+++ start.sh
@@ -5,27 +5,64 @@
 echo "Script ini dibuat untuk ubuntu 16.04"
 echo "script ini akan memulai konfigurasinya secara otomatis..."
 
-
+while true
+do
+ read -r -p "Apakah anda ingin menambah Repo Kambing? [Y/n] " input
+ 
+ case $input in
+     [yY][eE][sS]|[yY])
+ echo "Yes"
 echo "Menambah Repository Kambing"
+ex -sc '1i|#' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://kambing.ui.ac.id/ubuntu xenial-proposed main restricted universe multiverse' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://kambing.ui.ac.id/ubuntu xenial-backports main restricted universe multiverse' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://kambing.ui.ac.id/ubuntu xenial-security main restricted universe multiverse' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://kambing.ui.ac.id/ubuntu xenial-updates main restricted universe multiverse' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://kambing.ui.ac.id/ubuntu xenial main restricted universe multiverse' -cx /etc/apt/sources.list
+ex -sc '1i|#' -cx /etc/apt/sources.list
+ex -sc '1i|#REPO KAMBING' -cx /etc/apt/sources.list
 
-echo "Menambah Repository Buaya"
+ ;;
+ 
+     [nN][oO]|[nN])
+ echo "No"
+        ;;
+ 
+     *)
+ echo "Invalid input..."
+ ;;
+ esac
+done
+####################################################################################################################################
+while true
+do
+ read -r -p "Apakah anda ingin menambah Repo Buaya? [Y/n] " input
+ 
+ case $input in
+     [yY][eE][sS]|[yY])
+ echo "Yes"
+ echo "Menambah Repository Buaya"
+ex -sc '1i|#' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://buaya.klas.or.id/ubuntu/ xenial-proposed main restricted universe multiverse' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://buaya.klas.or.id/ubuntu/ xenial-backports main restricted universe multiverse' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://buaya.klas.or.id/ubuntu/ xenial-security main restricted universe multiverse' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://buaya.klas.or.id/ubuntu/ xenial-updates main restricted universe multiverse' -cx /etc/apt/sources.list
 ex -sc '1i|deb http://buaya.klas.or.id/ubuntu/ xenial main restricted universe multiverse' -cx /etc/apt/sources.list
+ex -sc '1i|#' -cx /etc/apt/sources.list
+ex -sc '1i|#REPO BUAYA' -cx /etc/apt/sources.list
 
-echo "Menambah Repository ITB"
-ex -sc '1i|deb ftp://ftp.itb.ac.id/pub/ubuntu xenial-proposed main restricted universe multiverse' -cx /etc/apt/sources.list
-ex -sc '1i|deb ftp://ftp.itb.ac.id/pub/ubuntu xenial-backports main restricted universe multiverse' -cx /etc/apt/sources.list
-ex -sc '1i|deb ftp://ftp.itb.ac.id/pub/ubuntu xenial-security main restricted universe multiverse' -cx /etc/apt/sources.list
-ex -sc '1i|deb ftp://ftp.itb.ac.id/pub/ubuntu xenial-updates main restricted universe multiverse' -cx /etc/apt/sources.list
-ex -sc '1i|deb ftp://ftp.itb.ac.id/pub/ubuntu xenial main restricted universe multiverse' -cx /etc/apt/sources.list
+ ;;
+ 
+     [nN][oO]|[nN])
+ echo "No"
+        ;;
+ 
+     *)
+ echo "Invalid input..."
+ ;;
+ esac
+done
+
 echo "Penambahan Repository Selesai..."
 
 
